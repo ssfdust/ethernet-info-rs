658ab58cf9f892704a89a71987077fbe79ff8592
ssfdust <ssfdust@gmail.com>
2023-08-05 16:44:20 +0800

    Add more tests file

--- scripts/test-cov.sh
+++ scripts/test-cov.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 set -e
-rm -f profiles/*
+rm -rf profiles/*
 files=$(RUSTFLAGS="-C instrument-coverage" LLVM_PROFILE_FILE="profiles/network-%p-%m.profraw" cargo test --tests --no-run --message-format=json 2>/dev/null | jq -r "select(.profile.test == true) | .filenames[]" | grep -v dSYM -)
 RUSTFLAGS="-C instrument-coverage" LLVM_PROFILE_FILE="profiles/network-%p-%m.profraw" cargo test --tests
 llvm-profdata merge -sparse profiles/*.profraw -o profiles/network.profdata
@@ -20,4 +20,4 @@ llvm-cov show --format html $( \
 ) \
 --use-color --ignore-filename-regex='/.cargo/registry' --ignore-filename-regex='/.cargo/git' --ignore-filename-regex='/rustc/' \
 --instr-profile=profiles/network.profdata --show-instantiations --show-line-counts-or-regions > coverage.html
-rm -f profiles/*
+rm -rf profiles
